5bdafd26664782de20925792be35db1353731f1b
Saurav.Surinder <Saurav.Surinder@LT0098051.in.ril.com>
2019-07-03 16:28:37 +0530

    comitted

---
+++ MyProject/src/FirstClass.java
@@ -0,0 +1,13 @@
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int radius=5;
+		
+		double area = 3.14*radius*radius;
+		System.out.println("area="+area);
+
+	}
+
+}
